be012b84e3c4b4ed7805d89613ba13213db82c0b
Achal Agarwal <achalagarwal.01@gmail.com>
2019-03-22 08:25:08 +0530

    Initial Commit

--- chai/interactive/corpus_interactive.py
+++ chai/interactive/corpus_interactive.py
@@ -1,11 +1,10 @@
 from chai.src.corpus.Corpus import Corpus
-from chai.src.master.Match import MatchSku
 
 
 def main():
 
     print("Hi, Welcome to Chai Interactive")
-    print("You are in the 'Single Corpus' Interactive Mode")
+    print("You are in the 'Corpus' Interactive Mode")
     corpus_name = input("Please Enter the name of your Corpus")
     corpus = Corpus(corpus_name)
 
@@ -16,6 +15,24 @@ def main():
     while not standard_term == '':
         corpus.add_leader(standard_term)
         standard_term = input()
-    
+
+    print("Listed Below are the clusters in the Corpus, ", corpus.name)
+
+    for cluster in corpus.clusters:
+        print(cluster)
+
+    print("We will match your terms to clusters")
+    term = input("Enter term")
+
+    while not term == '':
+        term_cluster = corpus.match_term_cluster(term)
+        if not term_cluster:
+            print(term, ' was not matched with any existing clusters')
+
+        else:
+            print(term, ' was matched to cluster ', term_cluster.name)
+        term = input("Enter term")
 
 
+if __name__ == '__main__':
+    main()
